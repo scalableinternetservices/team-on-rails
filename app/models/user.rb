fdb2a57d5d9880828ef49c5f7341d2dd88665a6b
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2024-11-21 06:51:29 -0800

    Save local changes before revert

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,4 @@
 class User < ApplicationRecord
     has_many :posts
     has_and_belongs_to_many :meetings
-    
-    validates :role, inclusion: { in: ['student', 'instructor'] }
-    
-    def student?
-        role == 'student'
-    end
-    
-    def instructor?
-        role == 'instructor'
-    end
 end
